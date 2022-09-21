6b5bfaf4d0d82ebd46768fd6da53769aaa9bb05e
Leechae00 <111486824+Leechae00@users.noreply.github.com>
2022-09-21 23:47:08 +0900

    Add files via upload

---
+++ CondStatementEx3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CondStatementEx3 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("숫자를 입력해주세요 : ");
+		int str = scan.nextInt();
+		//int number = Integer.parseInt(str);
+		if (str%2 == 0) {
+			System.out.println("입력하신 숫자는 짝수입니다.");
+		}
+		else {
+			System.out.println("입력하신 숫자는 홀수입니다.");
+		}
+	}
+}
